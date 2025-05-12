7fa49043893de25c2f5f9d18b55da4db4cf934d1
Aastitva Jain <aastitva.jain@avalara.com>
2025-05-12 10:49:56 +0530

    Fixed token check

--- src/Connect.Infra.Data/Requests/Common/RequestStore.cs
+++ src/Connect.Infra.Data/Requests/Common/RequestStore.cs
@@ -11,7 +11,9 @@ internal class RequestStore(ConnectDbContext context)
     public async Task<bool> RequestExists(ClientToken token, CancellationToken cancellationToken)
     {
         return await context.Clients.AnyAsync(
-            x => x.ApproveRequests != null && x.ApproveRequests.Any(y => y.Status == RequestStatus.Pending),
+            x => 
+                x.Id == token.Id && x.Sequence == token.Sequence &&
+                x.ApproveRequests != null && x.ApproveRequests.Any(y => y.Status == RequestStatus.Pending),
             cancellationToken: cancellationToken);
     }
     
